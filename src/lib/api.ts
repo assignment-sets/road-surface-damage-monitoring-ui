6321ae105ed9de0b1dc6f9eb523bc53a414a9f59
assignment-sets <mondalgourab140@gmail.com>
2026-03-13 16:38:43 +0530

    feat: create apiClient using axios for API interactions

---
+++ src/lib/api.ts
@@ -0,0 +1,9 @@
+// src/lib/api.ts
+import axios from "axios";
+
+export const apiClient = axios.create({
+  baseURL: "http://localhost:8000/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
