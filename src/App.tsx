4e0f8ea8ef2665e3162cb8f978da55dcf6ad654a
assignment-sets <mondalgourab140@gmail.com>
2026-03-13 17:34:43 +0530

    feat: set up routing and layout for the application with Navbar and Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Toaster } from "sonner";
+
+import UploadPage from "./pages/UploadPage";
+import DashboardPage from "./pages/DashboardPage";
+import { Navbar } from "./components/layout/Navbar";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div className="min-h-screen bg-background text-foreground dark">
+        <Navbar />
+
+        <main className="pb-10 pt-4">
+          <Routes>
+            <Route path="/" element={<Navigate to="/upload" replace />} />
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </main>
+
+        <Toaster theme="dark" position="bottom-right" richColors />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
